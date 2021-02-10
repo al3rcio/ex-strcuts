66feb9c6667b56fd74d524ccbdbebfc2020d0df4
al3rcio <al3rcio@gmail.com>
2021-02-10 15:04:23 -0300

    -;-Esqueleto do programa

--- turma.c
+++ turma.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TURMA 20
+#define TURMA 2
+#define STR 20
 
+typedef struct
+{
+   int nota1;
+   int nota2;
+   int nota3;
+} notas;
+
+
+typedef struct
+{
+    char[STR] nome;
+    int matricula;
+    notas nota;
+} aluno;
+
+void InserindoDados();
+void ImprimeDados();
+aluno BuscaMatricula(int numeroMatricula);
+
+int main()
+{
+    InserindoDados();
+
+    return 0;
+}
+
+void InserindoDados()
+{
+    // TODO
+}
+
+void ImprimeDados()
+{
+    // TODO
+}
+
+aluno BuscaMatricula(int numeroMatricula)
+{
+    aluno dadosAluno;
+
+    // TODO
+
+    return dadosAluno
+}
