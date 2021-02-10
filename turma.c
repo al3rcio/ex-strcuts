a002aa97ab2287bd157c39cf3315f29f8160f5c5
al3rcio <al3rcio@gmail.com>
2021-02-10 19:03:29 -0300

    Funcionando CalculaMediaAluno e ImprimeMediaAluno

--- turma.c
+++ turma.c
@@ -3,12 +3,14 @@
 
 #define TURMA 2
 #define STR 20
+#define TOTAL 3
 
 typedef struct
 {
-   int nota1;
-   int nota2;
-   int nota3;
+   float nota1;
+   float nota2;
+   float nota3;
+   float media;
 } notas;
 
 typedef struct 
@@ -18,12 +20,16 @@ typedef struct
     notas nota;
 } aluno;
 
+// declarando funções
 void InsereDados();
 void ImprimeDados();
 aluno BuscaMatricula(int numeroMatricula);
 void InsereNotas();
-float CalculaMedia(notas notas);
+float CalculaMediaAluno(aluno notas[], int tamanhoTurma);
+void ImprimeMediaAluno(aluno notas[], int tamanhoTurma);
+float CalulaMediaTurma(float media[], int tamanhoTurma);
 aluno BuscaMatricula(int numeroMatricula);
+
 
 aluno turma[TURMA]; 
 
@@ -33,6 +39,10 @@ int main()
     printf("********\n");
     ImprimeDados();
     InsereNotas();
+    CalculaMediaAluno(turma, TURMA);
+    ImprimeMediaAluno(turma, TURMA);
+
+
     return 0;
 }
 
@@ -57,25 +67,59 @@ void ImprimeDados()
 }
 
 void InsereNotas()
-{     
+{
+    printf("#### INSIRA AS NOTAS ####\n");     
     for (int i = 0; i < TURMA; i++)
     {
         printf("Informe as notas 1, 2 e 3 do aluno %s\n", turma[i].nome);
         printf("Nota 1: ");
-        scanf("%i", &turma[i].nota.nota1);
+        scanf("%f", &turma[i].nota.nota1);
         printf("Nota 2: ");
-        scanf("%i", &turma[i].nota.nota2);
+        scanf("%f", &turma[i].nota.nota2);
         printf("Nota 3: ");
-        scanf("%i", &turma[i].nota.nota3);
+        scanf("%f", &turma[i].nota.nota3);
     }    
 }
 
-float CalculaMedia(notas notas)
+float CalculaMediaAluno(aluno notas[], int tamanhoTurma)
 {
-    float media;
-    
+    float soma[tamanhoTurma];
+    //float media[tamanhoTurma];
+    for (int i = 0; i < tamanhoTurma; i++)
+    {
+        soma[i] = notas[i].nota.nota1 + notas[i].nota.nota2 + notas[i].nota.nota3;
+        notas[i].nota.media = soma[i] / TOTAL;
+    }
 
-    return media;
+    return notas[tamanhoTurma].nota.media;
+}
+
+void ImprimeMediaAluno(aluno notas[], int tamanhoTurma)
+{
+   
+    for (int i = 0; i < 1; i++)
+    {
+        printf("Aluno \t Nota 1 \t Nota 2 \t Nota 3 \t Média\n");
+        for (int j = 0; j < tamanhoTurma; j++)
+        {
+            printf("%s \t %.2f \t\t %.2f \t\t %.2f \t\t %.2f \n", notas[j].nome, notas[j].nota.nota1, 
+                notas[j].nota.nota2, notas[j].nota.nota3, notas[j].nota.media);
+        }
+    }
+}
+
+float CalulaMediaTurma(float media[], int tamanhoTurma)
+{
+    float mediaTurma;
+    float soma = 0;
+
+    for (int i = 0; i < tamanhoTurma; i++)
+    {
+        soma = soma + media[i];
+    }
+
+    mediaTurma = soma / (float) tamanhoTurma;
+    return mediaTurma;
 }
 
 aluno BuscaMatricula(int numeroMatricula)
